6149c197663dc0a90044ab6590a0660200b530de
PurplePopo <1094147876@qq.com>
2019-09-11 11:28:22 +0800

    enter

---
+++ PopoGUI/PopoGUI_Device/PopoGUI_DeviceConfigure.h
@@ -0,0 +1,8 @@
+/*
+ * @Description: Configure your Device.
+ * @Author: PurplePopo
+ * @Github: https://github.com/PurplePopo/PopoGUI
+ * @Date: 2019-09-11 11:07:03
+ * @LastEditors:: PurplePopo
+ * @LastEditTime: 2019-09-11 11:07:52
+ */
