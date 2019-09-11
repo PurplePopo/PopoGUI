fe63cc9e5edaf7fb80b3f1908578ab2b502a5610
PurplePopo <1094147876@qq.com>
2019-09-11 10:30:53 +0800

    enter

---
+++ PopoGUI/PopoGUI_Device/PopoGUI_Device.h
@@ -0,0 +1,16 @@
+/*
+ * @Description: 
+ * @Author: PurplePopo
+ * @Github: https://github.com/PurplePopo/PopoGUI
+ * @Date: 2019-09-11 09:58:18
+ * @LastEditors:: PurplePopo
+ * @LastEditTime: 2019-09-11 10:29:39
+ */
+
+
+#ifndef __POPOGUI_DEIVICE_H__
+#define __POPOGUI_DEIVICE_H__
+
+
+
+#endif
